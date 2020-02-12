3df403505cf60eab3039b696cc26c684f244528d
Jude Ukana <Judeukana@gmail.com>
2020-02-12 15:49:57 +0100

    completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,11 +7,11 @@ export class ProductService {
 
   constructor(private _http : Http) { }
 
-  private _albumurl = '../assets/album.json' 
+  private _albumUrl = '../assets/album.json' 
 
 
   getAlbum(id :number){
     
-    return this._http.get(this._albumurl).map((response)=>response.json());
+    return this._http.get(this._albumUrl).map((response)=>response.json());
   }
 }
