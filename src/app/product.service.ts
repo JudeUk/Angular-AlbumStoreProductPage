ee6b3d07097a4ebcf7a2199f78fc8a9fe6c54884
Jude Ukana <Judeukana@gmail.com>
2020-02-12 15:29:45 +0100

    completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http, Response  } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http : Http) { }
+
+  private _albumurl = '../assets/album.json' 
+
+
+  getAlbum(id :number){
+    
+    return this._http.get(this._albumurl).map((response)=>response.json());
+  }
+}
